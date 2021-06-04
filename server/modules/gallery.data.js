10444fa285f4a6c2334c77fcd20aabbcc65ec1d7
Stephen Piper <stephen.r.piper@gmail.com>
2021-06-04 17:31:54 -0500

    added descriptions and what not

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,11 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/bitches-brew.jpg', description: 'Do you like 70s rock?  This album might be why.', likes: 0 },
+    { id: 2, path: 'images/cat-hiroshi.jpg', description: 'Funky Jazz Soul fusion with a wild trombone solos.', likes: 0 },
+    { id: 3, path: 'images/Getz-Gilberto.jpg', description: 'Smooth Portuguese vocals from Gilberto.', likes: 0 },
+    { id: 4, path: 'images/jazz-is-dead.jpg', description: 'A futuristic nod to the past.', likes: 0 },
+    { id: 5, path: 'images/love-supreme.jpg', description: 'An incantation.', likes: 0 }
+
+
 ];
 
 module.exports = galleryItems;
