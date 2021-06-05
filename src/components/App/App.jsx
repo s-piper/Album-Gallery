6f68c92675aaf1e65024096966501c89f1b6b8d5
Stephen Piper <stephen.r.piper@gmail.com>
2021-06-05 18:52:12 -0500

    added useeffect and made Get route

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 import './App.css';
+import {useState, useEffect} from 'react';
+import Axios from 'axios';
+
+
+
 
 function App() {
+
+  const [albumList, setAlbumList] =useState([]);
+
+  useEffect (()=> {
+    getAlbumList()
+  }, []);
+
+  const getAlbumList = () =>{
+    console.log('start of GET');
+    Axios.get ('/gallery').then(response => {
+      setAlbumList(response.data)
+    }).catch(err => {
+      console.log('error in GET', err);
+    })
+  }
+
+    console.log(albumList);
+
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Gallery of My Life</h1>
+          <h1 className="App-title">In My Jazz Rotation</h1>
         </header>
         <p>Gallery goes here</p>
         <img src="images/goat_small.jpg"/>
