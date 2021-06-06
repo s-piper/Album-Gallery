fe6973ee69535f53b57ab0903b392d13072faca0
Stephen Piper <stephen.r.piper@gmail.com>
2021-06-05 21:35:56 -0500

    added like button and put to make it work

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -4,13 +4,25 @@ import axios from 'axios';
 function GalleryItem({album, getAlbumList}) {
 
     const [isText, setIsText] = useState(false)
+
+    const handleLike = () =>{
+        axios.put(`/gallery/like/${album.id}`)
+        .then(response => {
+            console.log('Put res:',response);
+            getAlbumList();
+        }).catch(err=>{
+            console.log('Put error', err);
+        })
+    }
     
     return(
 
         <div className="pictureBox">
 
             <img src={album.path} width="200" height="200"/>
-            <button>Love It!</button>
+            <button onClick={handleLike}>Love It!</button>
+            <p>{album.likes} people love this!</p>
+
 
         </div>
        
